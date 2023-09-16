9628ddf400fd427face5d333db6fb190ad142845
GammachisT <gammachistesfaye@gmail.com>
2023-09-16 15:29:41 -0700

    C simple shell team project

--- shell.h
+++ shell.h
@@ -1,144 +1,199 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define _TRUE            1
-#define _FALSE           0
-
-#define STDIN            0
-#define STDOUT           1
-#define STDERR           2
-
-#define NON_INTERACTIVE  0
-#define INTERACTIVE      1
-
-#define PERMISSIONS      1
-#define NON_PERMISSIONS -1
-
-#define _FILE            10
-#define NON_FILE         -10
-
-#define _ENOENT          "No such file or directory"
-#define _EACCES          "Permission denied"
-#define _CMD_NOT_EXISTS  "not found"
-#define _ILLEGAL_NUMBER  "Illegal number"
-
-#define _CODE_ENOENT           3
-#define _CODE_EACCES           13
-#define _CODE_CMD_NOT_EXISTS   132
-#define _CODE_ILLEGAL_NUMBER   133
-typedef struct __attribute__((__packed__))
-{
-	char *command;            /* Command to execute */
-	int n_commands;           /* Number of commands executed */
-	char *utility_path;         /* Path of a command */
-	int current_path;      /* Check if is current path or not */
-	int status_code;          /* Last exit code */	
-	int argc;                 /* Number of arguments received */
-	char **argv;              /* Arguments received */
-	int mode;                 /* INTERACTIVE or NON_INTERACTIVE */
-	int error_code;           /* Error code for error message */
-	char *buffer;             /* Line readed with the getline */
-	char **arguments;         /* Line splited into words */
-	char *environment;        /* Last environment variable get it */
-	int pid;                  /* Process id */
-} shell_t;
-
-typedef struct __attribute__((__packed__))
-{
-	char *sms;            /* Error message */
-	int code;                 /* Error code for identify the error message */
-} error_t;
-
-typedef struct __attribute__((__packed__))
-{
-	char *cmd;            /* arguments[0] = cmd */
-	void (*func)(shell_t *info, char **arguments);
-} builtin_t;
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
 
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
+/* Points to an array of pointers to strings called the "environment" */
 extern char **environ;
 
-/* cmds.c */
-void analyze(char **arguments, shell_t *info, char *buff);
+typedef struct data
+{
+	char **av;			/* argument vector */
+	char *input;			/* command line written by the user */
+	char **args;			/* tokens of the command line */
+	int status;			/* last status of the shell */
+	int counter;			/* lines counter */
+	char **_environ;		/* environment variable */
+	char *pid;			/* process ID of the shell */
+} shell_t;
 
-/* permissions.c */
-int executable(char *filename);
-int is_file(char *command);
+/* struct sep_list_s - single linked list */
+typedef struct sep_list_s
+{
+	char separator;			/*  @separator: ; | & */
+	struct sep_list_s *next;
+} sep_list;
 
-/* environ.c */
-void shell_env(shell_t *info, char **arguments);
-char *_getenv(const char *name);
-char *find_dir(char *filename, shell_t *info);
-void current_path(char *path, shell_t *info);
-void get_environment(void);
+/* struct line_list_s - single linked list */
+typedef struct line_list_s
+{
+	char *line;				/* command line */
+	struct line_list_s *next;		/* single linked list */
+} line_list;
 
-/* execute.c */
-void execute(char *commands, char **arguments, shell_t *info, char *buff);
-int current_directory(char *cmd, char **arguments, char *buff,
-		shell_t *info);
+/* struct r_var_list - single linked list */
+typedef struct r_var_list
+{
+	int len_var;
+	char *val;
+	int len_val;
+	struct r_var_list *next;
+} r_var;
 
-/* error.c */
-char *msg_match(shell_t info);
-void error(shell_t *info);
-void error_extra(shell_t *info, char *extra);
+/** struct builtin_s - Builtin struct for command args. */
+typedef struct builtin_s
+{
+	char *name;			/* name of command builtin i.e cd, exit,env */
+	int (*f)(shell_t *info);
+} builtin_t;
 
+/* lists.c */
+sep_list *add_sep_node_end(sep_list **head, char sep);
+void free_sep_list(sep_list **head);
+line_list *add_line_node_end(line_list **head, char *line);
+void free_line_list(line_list **head);
 
-/* builtins.c */
-int builtins(shell_t *info, char **arguments);
-int check_builtin(shell_t *info, char **arguments);
-
-/* exit.c */
-void shell_exit(shell_t *info, char **arguments);
-int number_controller(shell_t *info, char *number);
-
-/* Entry point of the shell */
-void start(shell_t *info);
-
-/* memory.c */
-void *_realloc(void *ptr, size_t old_size, size_t new_size);
-void free_memory_p(void *ptr);
-void free_memory_pp(void **ptr);
-
-/* prompt.c */
-void get_prompt(shell_t *info);
-void prompt(shell_t *info);
-char *read_prompt();
-void sigHandler(int sig_num);
+/* lists1.c */
+r_var *add_rvar_node(r_var **head, int lvar, char *var, int lval);
+void free_rvar_list(r_var **head);
 
 /* strings.c */
-int _strlen(char *msg);
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, const char *src);
 char *_strcpy(char *dest, char *src);
-char *_strdup(char *str);
-int _strcmp(char *str1, char *str2);
-char *to_string(int number);
-int is_digit(unsigned int n);
-int _atoi(char *s);
-int is_alpha(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
 
-/* printout.c */
-int _putchar_to_fd(char l, int fd);
-int print_to_fd(char *msg, int fd);
-int _putchar(char c);
-int print(char *msg);
-int print_err(char *msg);
+/* memory.c */
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
+
+/* strings2.c */
+char *_strdup(const char *s);
+int _strlen(const char *s);
+int cmp_chars(char str[], const char *delim);
+char *_strtok(char str[], const char *delim);
+
+/* strings1.c */
+int _isdigit(const char *s);
+void rev_string(char *s);
+
+/* syntax.c */
+int repeated_char(char *input, int i);
+int error_sep_op(char *input, int i, char last);
+int first_char(char *input, int *i);
+void print_syntax_error(shell_t *info, char *input, int i, int bool);
+int check_syntax_error(shell_t *info, char *input);
+
+/* shell_loop.c */
+char *without_comment(char *in);
+void shell_loop(shell_t *info);
 
 /* parser.c */
-char **tokenize(char *line, const char *sep);
-char *join_tokens(char *word1, char *word2, char *word3, const char *sep);
+char *swap_chars(char *input, int bool);
+void add_nodes(sep_list **head_s, line_list **head_l, char *input);
+void go_next_cmd(sep_list **list_s, line_list **list_l, shell_t *info);
+int tokenize(shell_t *info, char *input);
+char **split_line(char *input);
 
-/* patterns.c */
-void test_patterns(shell_t *info, char **arguments);
-char *pattern_handler(shell_t *info, char *string);
-char *replace_value(shell_t *info, int *index, char *string);
-char *replacement(shell_t *info, int *index, char *string);
-char *replace_env(shell_t *info, char *environment);
+/* readline.c */
+char *read_line(int *i_eof);
+void check_env(r_var **h, char *in, shell_t *info);
+int check_vars(r_var **h, char *in, char *st, shell_t *info);
+char *replaced_input(r_var **head, char *input, char *new_input, int nlen);
+char *rep_var(char *input, shell_t *info);
 
-#endif /* SHELL_H */
+/* getline.c */
+void assign_line(char **lineptr, size_t *n, char *buffer, size_t j);
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
+
+/* executing.c */
+int exec_cmd(shell_t *info);
+
+/* cmds.c */
+int is_cdir(char *path, int *i);
+char *_which(char *cmd, char **_environ);
+int is_executable(shell_t *info);
+int check_error_cmd(char *dir, shell_t *info);
+int cmd_exec(shell_t *info);
+
+/* environ.c */
+char *_getenv(const char *name, char **_environ);
+int cmp_env_name(const char *nenv, const char *name);
+int print_env(shell_t *info);
+char *copy_info(char *name, char *value);
+
+/* set_env */
+void set_env(char *name, char *value, shell_t *info);
+int _setenv(shell_t *info);
+int _unsetenv(shell_t *info);
+
+/* cd.c */
+void cd_dot(shell_t *info);
+void cd_to_dir(shell_t *info);
+void cd_to_pre(shell_t *info);
+void cd_to_home(shell_t *info);
+int change_cd(shell_t *info);
+
+/* builtins.c */
+int (*get_builtin(char *cmd))(shell_t *info);
+
+/* exit.c */
+int exit_shell(shell_t *info);
+char *exit_shell_error(shell_t *info);
+void help_exit(void);
+
+/* strdlib.c */
+int get_len(int n);
+char *aux_itoa(int n);
+int _atoi(char *s);
+
+/* get_error.c */
+int get_error(shell_t *info, int eval);
+
+/* errors.c */
+char *strcat_cderror(shell_t *, char *, char *, char *);
+char *get_cd_error(shell_t *info);
+char *error_not_found(shell_t *info);
+char *path_error_126(shell_t *info);
+char *env_error(shell_t *info);
+
+/* get_sigint.c */
+void get_sigint(int sig);
+
+/* help_patterns.c */
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help(void);
+void help_cd(void);
+
+/* get_help.c */
+int get_help(shell_t *info);
+void help_alias(void);
+void general_help(void);
+
+/* shell.c */
+void set_data(shell_t *info, char **av);
+void free_data(shell_t *info);
+int main(int ac, char **av);
+
+char *error_get_alias(char **args);
+char *error_syntax(char **args);
+char *error_permission(char **args);
+
+#endif
